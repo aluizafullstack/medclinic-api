260c456cfe662ada3c854909c46f38a5db3eec2b
Ana Luiza <a.luiza.fullstack@gmail.com>
2026-09-10 07:38:28 -0300

    feat: inicializa datasource antes de subir o servidor

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
+import { AppDataSource } from "./database/data-source";
 
-// Criando a instancias
+// Criando a instancia do Express
 const app = express();
 const PORT = 3000;
 
@@ -12,12 +13,19 @@ app.get("/health-check", (req: Request, res: Response) => {
   });
 });
 
-// Rota desconhecida 404
+// Rota desconhecida -> 404
 app.use((_: unknown, res: Response) =>
   res.status(404).json({ error: "Rota não encontrada" }),
 );
 
-// Iniciando o servidor
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor da MedClinic API rodando na porta ${PORT}`);
-});
+// Conecta ao banco de dados e só depois Inicia o servidor
+AppDataSource.initialize()
+  .then(() => {
+    console.log("✅ Conexão com o banco de dados da MedClinic estabelecida");
+    app.listen(PORT, () => {
+      console.log(`🚀 Servidor da MedClinic API rodando na porta ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("❌ Erro ao conectar com o banco de dados:", error);
+  });
