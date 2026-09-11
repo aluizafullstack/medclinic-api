a59f149a5b12e769663e7c2c8fc179f084f0f142
Ana Luiza <a.luiza.fullstack@gmail.com>
2026-09-10 21:44:41 -0300

    feat: implementa logica de login com emissao de token jwt

--- src/services/AutenticacaoService.ts
+++ src/services/AutenticacaoService.ts
@@ -3,12 +3,15 @@ import { IUsuarioRepository } from "../repositories/IUsuarioRepository";
 import { RegistrarUsuarioDto } from "../dtos/RegistrarUsuarioDto";
 import { AppError } from "../errors/AppError";
 import { gerarHashSenha } from "../utils/senhaHash";
+import { LoginDto } from "../dtos/LoginDto";
+import { LoginResponseDto } from "../dtos/LoginResponseDto";
+import { gerarToken } from "../utils/jwt";
+import { compararSenha } from "../utils/senhaHash";
 
 /*
     Esse AutenticacaoService é responsável pelas regras de negócio
     relacionadas à autenticação dos usuários (cadastro e login).
 */
-
 // Expressão regular usada para validar o formato básico de um e-mail
 const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 export class AutenticacaoService {
@@ -46,6 +49,40 @@ export class AutenticacaoService {
       email: usuario.email,
       role: usuario.role,
       criadoEm: usuario.criadoEm,
+    };
+  }
+
+  // Metdod responsável por autenticar um usuario já cadastrado
+  async login(data: LoginDto): Promise<LoginResponseDto> {
+    const { email, senha } = data;
+
+    if (!email || !senha) {
+      throw new AppError("E-mail e senha obrigatório", 400);
+    }
+
+    const usuario = await this.usuarioRepository.findByEmail(email);
+    if (!usuario) {
+      throw new AppError("Credenciais inválidas", 401);
+    }
+
+    const senhaValida = await compararSenha(senha, usuario.senha);
+    if (!senhaValida) {
+      throw new AppError("Credenciais inválidas", 401);
+    }
+
+    const token = gerarToken({
+      id: usuario.id,
+      role: usuario.role,
+    });
+
+    return {
+      token,
+      usuario: {
+        id: usuario.id,
+        nome: usuario.nome,
+        email: usuario.email,
+        role: usuario.role,
+      },
     };
   }
 }
