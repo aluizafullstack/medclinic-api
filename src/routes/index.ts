ef86bb7c76c247a4d9aafb27c033e480993e03d7
Ana Luiza <a.luiza.fullstack@gmail.com>
2026-09-11 21:24:37 -0300

    feat: registra rotas de admin no router agregador

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import autenticacaoRoutes from "./AutenticacaoRoutes";
 import usuarioRoutes from "./UsuarioRoutes";
+import adminRoutes from "./AdminRoutes";
 
 /*
     Esse index é o agregador de rotas.
@@ -11,5 +12,6 @@ const router = Router();
 
 router.use("/autenticacao", autenticacaoRoutes);
 router.use("/usuarios", usuarioRoutes);
+router.use("/admin", adminRoutes);
 
 export default router;
